56da8e103498c30f295cc6294e9e9548ffdfc0be
Konrad-Ł <lysek.konrad@gmail.com>
2023-01-13 23:07:09 +0100

    Added Hard Desert

---
+++ Assets/Scripts/ProcTerrain.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcTerrain : MonoBehaviour
+{
+   // private int[][] coords = new int[100][100];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    //for()    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
